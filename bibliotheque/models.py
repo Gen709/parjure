e23925088df54b06c0e2a7023e12f16fb8f544dd
Gen709 <709ruebeaudoin@gmail.com>
2022-09-22 10:05:11 -0400

    added default value to parent in bibliotheque model

--- bibliotheque/models.py
+++ bibliotheque/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class library(models.Model):
-    parent = models.IntegerField()
+    parent = models.IntegerField(null=True, default=True)
     lft = models.IntegerField()
     rgt = models.IntegerField()
     item = models.CharField(max_length=250, blank=True, null=True, default=None)
