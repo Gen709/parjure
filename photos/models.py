58a800ffa674bf9bf69211171557df8def37aff4
Gen709 <709ruebeaudoin@gmail.com>
2022-09-27 11:45:20 -0400

    modif de l'app photos

--- photos/models.py
+++ photos/models.py
@@ -9,11 +9,12 @@ class PhotosLibrary(models.Model):
     file = models.ImageField(upload_to='images/', blank=True, null=True)
     caption = models.CharField(max_length=100, blank=True, null=True)
     group = models.CharField(max_length=100, blank=True, null=True)
-    headline=models.CharField(max_length=100, blank=True, null=True)
-    text=models.CharField(max_length=100, blank=True, null=True)
+    headline = models.CharField(max_length=100, blank=True, null=True)
+    text = models.CharField(max_length=100, blank=True, null=True)
+
 
 class TimelineEra(models.Model):
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
-    text_title  = models.CharField(max_length=100)
-    text_text = models.CharField(max_length=100)
+    text_headline = models.CharField(max_length=100)
+    text_text = models.TextField(null=True, blank=True)
